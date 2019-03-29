e0dae1178c4026aae0c0fda76ca5be6bf970034a
Unknown <egor9996@gmail.com>
2019-03-29 11:28:31 +0300

    full valid algos

--- Divide-and-conquer/ClassLibraryDaC/ClosestPointPair.cs
+++ Divide-and-conquer/ClassLibraryDaC/ClosestPointPair.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Linq; 
+using System.Threading.Tasks; 
 
 namespace ClassLibraryDaC
-{ 
+{
+
+    /// <summary>
+    /// Структура точки: координаты x,y и ее номер 
+    /// </summary>
+    public struct PointId
+    {
+        public int x, y, id;
+    } 
 
     public class ClosestPointPair
     {
@@ -20,7 +27,7 @@ namespace ClassLibraryDaC
         /// </summary>
         static double MinDist;
 
-        /// <summary>
+        /// <summary> 
         /// Номер первой точки с минимальной дистанцией 
         /// </summary>
         static int AnswerA;
@@ -30,28 +37,64 @@ namespace ClassLibraryDaC
         /// </summary>
         static int AnswerB;
          
-        /// <summary>
-        /// Структура точки: координаты x,y и ее номер 
-        /// </summary>
-        public struct PointId
-        { 
-            public int x, y, id;
-        } 
 
         /// <summary>  
-        /// Массив точек  
+        /// Массив точек   
         /// </summary>  
-        public PointId[] a = new PointId[MAXN];
+        public PointId[] a;
+
+        public ClosestPointPair(PointId[] A)
+        {
+            MinDist = 1e20;
+ 
+            a = A; 
 
 
+        }
+
+        /// <summary>
+        /// Нахождение двух ближайших пар точек 
+        /// </summary>
+        /// <returns></returns>
+        public PointId[] NearestPair() 
+        {
+            List<PointId> NearestPair = new List<PointId>(); 
+
+            SortByX(a, 0 ,a.Length - 1); 
+            Rec(0, a.Length - 1);  
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                 if ((a[i].id == AnswerA) || (a[i].id == AnswerB))
+                 {
+                    NearestPair.Add(a[i]);
+                 } 
+            } 
+
+            return NearestPair.ToArray(); 
+        }
+
+        /// <summary>
+        /// Нахождение минимального расстояния между двумя точками 
+        /// </summary>
+        /// <returns></returns>
+        public double MinDistance()  
+        {
+            SortByX(a, 0, a.Length - 1);
+            Rec(0, a.Length - 1);
+              
+            return Math.Round(MinDist,3);
+        }
+
+         
         /// <summary>
         /// Сортировка массива точек по x-координатам
         /// </summary>
         /// <param name="a"></param>
         /// <param name="first"></param>
-        /// <param name="last"></param>
+        /// <param name="last"></param> 
         /// <returns></returns>
-        protected static PointId[] SortByX(PointId[] a, int first, int last)
+        public static PointId[] SortByX(PointId[] a, int first, int last)
         {
             List<PointId> OrderedArr = new List<PointId>(); // список для сортировки 
 
@@ -67,12 +110,12 @@ namespace ClassLibraryDaC
 
         /// <summary> 
         /// Сортировка массива точек по y-координатам 
-        /// </summary> 
+        /// </summary>  
         /// <param name="a"></param>
         /// <param name="first"></param>
         /// <param name="last"></param>
         /// <returns></returns>
-        protected static PointId[] SortByY(PointId[] a, int first, int last)
+        public static PointId[] SortByY(PointId[] a, int first, int last)
         {
             List<PointId> OrderedArr = new List<PointId>(); // список для сортировки 
 
@@ -88,11 +131,11 @@ namespace ClassLibraryDaC
 
 
         /// <summary>
-        /// Вычисление расстояния между точками 
+        /// Вычисление расстояния между точками  
         /// </summary>
         /// <param name="a"></param>
         /// <param name="b"></param>
-        private static void CalculateDist(PointId a, PointId b) 
+        public static void CalculateDist(PointId a, PointId b) 
         {
             double Dist = Math.Sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + .0); // вычисляем расстояние
 
@@ -104,15 +147,15 @@ namespace ClassLibraryDaC
                 AnswerB = b.id;
             }
                
-        }
+        } 
 
         /// <summary>
         /// Рекурсивный алгоритм, работает с массивом a[l...r]
         /// </summary>
         /// <param name="l">Левая граница массива</param>
         /// <param name="r">Правая граница массива</param> 
-        private void rec(int l , int r) 
-        { 
+        private void Rec(int l , int r) 
+        {   
             // Если расстояние между r и l слишком мало, то рекурсию надо остановить, 
             // и выполнить тривиальный алгоритм поиска ближайшей пары 
             // и затем отсортировать подмассив по y - координате
@@ -126,82 +169,46 @@ namespace ClassLibraryDaC
                 return;
             }
 
-            PointId[] t = new PointId[MAXN]; //вспомогательные буферы  
-            List<PointId> b = new List<PointId>(); // B = {Pi| |Xi - midx| < Mindist}
-            List<PointId> c = new List<PointId>(); // C(Pi) = {Pj | Pj (- B, Yi - Mindist < Yj <= Yi}
-             
-
+            // PointId[] B = new PointId[MAXN]; // B = {Pi| |Xi - midx| < Mindist}
+            List<PointId> b = new List<PointId>(); 
+               
             int mid = l + r / 2; // индекс середины массива 
             int midx = a[mid].x;   // x-координата середины массива 
-            rec(l, mid);  
-            rec(mid + 1, r); // рекурсивный обход правой и левой половины массива 
+            Rec(l, mid);  
+            Rec(mid + 1, r); // рекурсивный обход правой и левой половины массива 
 
-             
-            // заполняем B
+
+
+            // заполняем B 
             for (int i = 0; i < a.Length; i++)
             {
                 // множество точек B = {Pi| |Xi - midx| < Mindist} 
                 if (Math.Abs(a[i].x - midx) < MinDist)
-                {
-                    b.Add(a[i]);    
+                { 
+                    b.Add(a[i]); 
                 }
             }
 
             PointId[] B = b.ToArray(); 
-                 
+             
             // сортируем     
-            SortByY(B, 0, B.Length); 
+            SortByY(B, 0, B.Length);
 
-            // Заполняем С  
-            for (int i = 0; i < B.Length; i++) 
-            {
-                for (int j = 0; j < B.Length; j++) 
-                {
-                     if (B[i].y - MinDist < B[j].y  && B[j].y <= B[i].y)
-                     {
-                        c.Add(B[j]);   
-                     }
-                } 
-            }
-
-            PointId[] C = c.ToArray(); 
-
-            //для каждой точки p_i \in B рассмотреть все точки p_j \in C(p_i), 
-            //и каждой пары(p_i, p_j) посчитать расстояние и сравнить с текущим наилучшим расстоянием.
+            // сравниваем все точки B, каждую с каждой, выделяем множество С(Pi) для каждой точки, считаем расстояние 
             for (int i = 0; i < B.Length; i++)
             {
-                for (int j = 0; j < C.Length; j++)
+                for (int j = 0; j < B.Length; j++)
                 {
-                    CalculateDist(B[i], C[j]);
-                }
-            }
+                    // C(Pi) = {Pj | Pj (- B, Yi - Mindist < Yj <= Yi} 
+                    if (B[i].y - MinDist < B[j].y && B[j].y <= B[i].y)
+                    { 
+                        if (B[i].id != B[j].id) // если точки не одинаковые(расстояние между ними 0)       
+                            CalculateDist(B[i], B[j]);
+                    } 
+                } 
+            } 
              
-
-            //// объединяем а1 и а2 в t и сортируем 
-            //t = a1.Concat(a2).ToArray();
-            //SortByY(t, 0, t.Length - 1);  
-
-            ////Для слияния двух множеств точек, полученных от рекурсивных вызовов, в одно(упорядоченное по y - координате),
-            ////мы используем стандартную функцию STL merge(), и создаём вспомогательный буфер t[](один на все рекурсивные вызовы).
-            ////merge(a + l, a + m + 1, a + m + 1, a + r + 1, t, &cmp_y); 
-            ////copy (t, t+r-l+1, a+l);  
-
-
-            //// не ебу че происходит надо разобраться 
-            //int tsz = 0; 
-            //for (int i = l; i <= r; ++i) 
-            //{
-            //    if (Math.Abs(a[i].x - midx) < MinDist)
-            //    {
-            //        for (int j = tsz - 1; j >= 0 && a[i].y - t[j].y < MinDist; --j)
-            //            CalculateDist(a[i], t[j]);
-            //        t[tsz++] = a[i];
-            //    } 
-            //}
-
-        }   
-
-
-
+ 
+        }    
     } 
 }
--- Divide-and-conquer/UnitTestProject/UnitTest1.cs
+++ Divide-and-conquer/UnitTestProject/UnitTest1.cs
@@ -5,12 +5,75 @@ using ClassLibraryDaC;
 namespace UnitTestProject
 {
     [TestClass]
-    public class Sorts 
+    public class Distance  
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod1()  
         {
-            
-        } 
+
+            PointId[] Arr = new PointId[] {
+
+                 new PointId() { x = -4, y = 3, id = 0 },
+                 new PointId() { x = -4, y = -3, id = 1 },
+                 new PointId() { x = 0, y = 0, id = 2 },
+                 new PointId() { x = 1, y = -1, id = 3 },
+                 new PointId() { x = 1, y = 3, id = 4 },
+                 new PointId() { x = 5, y = -3, id = 5 },
+            };
+             
+            ClosestPointPair points = new ClosestPointPair(Arr);
+
+            double MinDist = points.MinDistance(); 
+             
+            Assert.AreEqual(1.414, MinDist);  
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            PointId[] Arr = new PointId[] {
+
+                 new PointId() { x = -4, y = 3, id = 0 },
+                 new PointId() { x = -4, y = -3, id = 1 },
+                 new PointId() { x = 0, y = 0, id = 2 },
+                 new PointId() { x = 1, y = -1, id = 3 },
+                 new PointId() { x = 1, y = 3, id = 4 },
+                 new PointId() { x = 5, y = -3, id = 5 },
+            };
+
+            ClosestPointPair points = new ClosestPointPair(Arr);
+
+            double MinDist = points.MinDistance();
+
+            Assert.AreEqual(1.414, MinDist);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+            PointId[] Arr = new PointId[] {
+
+                 new PointId() { x = 3, y = 6, id = 0 },
+                 new PointId() { x = -5, y = 5, id = 1 },
+                 new PointId() { x = 3, y = 3, id = 2 }, 
+                 new PointId() { x = -5, y = -3, id = 3 },  
+                 new PointId() { x = -3, y = 2, id = 4 }, 
+                 new PointId() { x = -1, y = 4, id = 5 },
+                 new PointId() { x = 3, y = 2, id = 6 }, 
+                 new PointId() { x = 6, y = 3, id = 7 },
+                 new PointId() { x = 0, y = 4, id = 8 }, 
+ 
+            };
+
+            ClosestPointPair points = new ClosestPointPair(Arr);
+
+            double MinDist = points.MinDistance();
+
+            Assert.AreEqual(1, MinDist); 
+        }
+
+
     }
-}
+}     
